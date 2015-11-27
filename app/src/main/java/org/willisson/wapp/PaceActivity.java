2c213d85ec8d86c5bc1be3bf6a798336452d0a6f
Pace Willisson <pace@alum.mit.edu>
2015-11-27 13:30:16 -0500

    pkts

--- app/src/main/java/org/willisson/wapp/PaceActivity.java
+++ app/src/main/java/org/willisson/wapp/PaceActivity.java
@@ -20,6 +20,7 @@ import java.net.MulticastSocket;
 public class PaceActivity extends AppCompatActivity {
     public DatagramSocket socket;
     public Thread timer;
+    public boolean timer_keep_running;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,15 +44,23 @@ public class PaceActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onDestroy() {
+        timer_keep_running = false;
+        super.onDestroy();
+
+    }
+
     void timer_setup() {
         Log.i("WAPP", "timer_setup");
         if (timer == null) {
+            Log.i ("WAPP", "creating new timer");
             timer = new Thread() {
                 public void run() {
                     udp_setup ();
 
                     try {
-                        for (; ; ) {
+                        while (timer_keep_running) {
                             Log.i("WAPP", "tick");
                             udp_tick ();
                             Thread.sleep(1000);
@@ -59,13 +68,17 @@ public class PaceActivity extends AppCompatActivity {
                     } catch (Exception e) {
                         Log.i("WAPP", "thread error " + e);
                     }
+                    Log.i ("WAPP", "timer done");
                 }
             };
+            Log.i ("WAPP", "starting timer");
+            timer_keep_running = true;
             timer.start();
         }
     }
 
     void udp_setup () {
+        Log.i ("WAPP", "doing udp_setup");
         try {
             WifiManager wifi = (WifiManager)getSystemService(Context.WIFI_SERVICE);
             WifiManager.MulticastLock lock = wifi.createMulticastLock ("WAPP");
@@ -73,20 +86,30 @@ public class PaceActivity extends AppCompatActivity {
             Log.i("WAPP", "got lock");
 
             if (socket == null) {
+                Log.i ("WAPP", "creating multicast socket");
                 socket = new MulticastSocket(20151);
-                InetSocketAddress addr = new InetSocketAddress("224.0.0.1", 20151);
-                Log.i("WAPP", "addr" + addr);
+
+                InetSocketAddress addr;
+                addr = new InetSocketAddress("224.0.0.1", 20151);
                 socket.connect(addr);
-                Log.i("WAPP", "connect done");
+                if (socket.getLocalAddress().getAddress()[0] == 10) {
+                    Log.i ("WAPP", "setup for emulator");
+                    socket.close();
+                    socket = new DatagramSocket(20151);
+                    addr = new InetSocketAddress("192.168.1.157", 20151);
+                    socket.connect(addr);
+                }
             }
        } catch (Exception e) {
             Log.i("WAPP", "udp error " + e);
         }
     }
+    public int tick_count;
 
     void udp_tick () {
         try {
-            String msg = "hello";
+            tick_count++;
+            String msg = "hello " + socket.getLocalAddress() + " " + timer + " " + tick_count;
             byte[] xbytes = msg.getBytes();
             DatagramPacket xpkt = new DatagramPacket(xbytes, xbytes.length);
             socket.send(xpkt);
