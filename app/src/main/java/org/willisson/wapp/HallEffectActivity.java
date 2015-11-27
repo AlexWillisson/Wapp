d6ee98ad20b6653335e792f2a353f47606fe05f5
Eric Willisson <ericwillisson@gmail.com>
2015-11-26 22:56:34 -0500

    Hall effect sensor activity

---
+++ app/src/main/java/org/willisson/wapp/HallEffectActivity.java
@@ -0,0 +1,38 @@
+package org.willisson.wapp;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class HallEffectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_hall);
+
+	final TextView text_view = (TextView) findViewById(R.id.output);
+
+	SensorManager sm = (SensorManager) getApplicationContext().getSystemService(Context.SENSOR_SERVICE);
+	sm.registerListener(new SensorEventListener() {
+		public void onAccuracyChanged(Sensor sensor, int accuracy) { }
+		public void onSensorChanged(SensorEvent event) {
+		    //		    StringBuilder sb = new StringBuilder();
+		    double strength = 0;
+		    for (int i = 0; i < 3; i++) {
+			//			sb.append(event.values[i] + "\n");
+			strength += event.values[i] * event.values[i];
+		    }
+		    strength = Math.sqrt(strength);
+		    //		    text_view.setText(sb.toString());
+		    text_view.setText(Double.toString(strength));
+		}
+	    }, sm.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD), 100000);
+    }
+}
--- app/src/main/java/org/willisson/wapp/MainActivity.java
+++ app/src/main/java/org/willisson/wapp/MainActivity.java
@@ -1,7 +1,9 @@
 package org.willisson.wapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+    }
+
+    public void startHallEffect(View v) {
+	Intent intent = new Intent(this, HallEffectActivity.class);
+	startActivity(intent);
     }
 }
