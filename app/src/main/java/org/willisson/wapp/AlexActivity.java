9477828985d0f341c433f03c33f01b495a1ad9b2
Alex Willisson <atw@alum.mit.edu>
2015-11-27 16:50:23 -0500

    notifications

--- app/src/main/java/org/willisson/wapp/AlexActivity.java
+++ app/src/main/java/org/willisson/wapp/AlexActivity.java
@@ -1,15 +1,22 @@
 package org.willisson.wapp;
 
+import android.app.NotificationManager;
+import android.app.Notification;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 public class AlexActivity extends AppCompatActivity {
+	NotificationManager nm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +26,29 @@ public class AlexActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+		nm = (NotificationManager) getSystemService (Context.NOTIFICATION_SERVICE);
     }
 
 	public void toast_btn (View view) {
-		send_toast ("hello, world");
+		send_toast("hello, world");
+	}
+
+	public void start_service (View view) {
+		send_toast ("notifying");
+		Notification.Builder builder = new Notification.Builder (this)
+			.setSmallIcon (R.mipmap.ic_launcher)
+			.setContentTitle ("foo")
+			.setContentText ("bar baz");
+		Notification notification = builder.build ();
+		nm.notify (9999, notification);
+		Log.i ("WAPP", "notified!");
 	}
 
 	public void send_toast (String text) {
 		Context context = getApplicationContext ();
 		int duration = Toast.LENGTH_SHORT;
 		Toast toast = Toast.makeText (context, text, duration);
-		toast.show ();
+		toast.show();
 	}
 }
