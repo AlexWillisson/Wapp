b713d0a5a2a707969e8d164574fe31c201a4cc2b
Alex Willisson <atw@alum.mit.edu>
2015-11-27 12:23:39 -0500

    toast

--- app/src/main/java/org/willisson/wapp/AlexActivity.java
+++ app/src/main/java/org/willisson/wapp/AlexActivity.java
@@ -1,11 +1,13 @@
 package org.willisson.wapp;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 public class AlexActivity extends AppCompatActivity {
 
@@ -17,6 +19,11 @@ public class AlexActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
 
+		Context context = getApplicationContext ();
+		CharSequence text = "hello world";
+		int duration = Toast.LENGTH_SHORT;
+		Toast toast = Toast.makeText (context, text, duration);
+		toast.show();
+    }
 }
