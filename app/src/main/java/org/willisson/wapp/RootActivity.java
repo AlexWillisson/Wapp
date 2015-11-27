adcee2a386948d3cd813b5e7c0a1ab4523746736
Alex Willisson <atw@alum.mit.edu>
2015-11-26 20:15:38 -0500

    split off activity

--- app/src/main/java/org/willisson/wapp/RootActivity.java
+++ app/src/main/java/org/willisson/wapp/RootActivity.java
@@ -2,6 +2,8 @@ package org.willisson.wapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
 
 public class RootActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class RootActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_root);
+    }
+
+    public void start_alex (View view) {
+	Intent intent = new Intent (this, AlexActivity.class);
+	startActivity(intent);
     }
 }
